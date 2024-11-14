45a36dce3d61f0f369782f3a2eb37f6bd33826a0
aixatatiane <tatiane.f@escolar.ifrn.edu.br>
2024-11-14 20:36:35 -0300

    Primeiro commit

--- projeto/settings.py
+++ projeto/settings.py
@@ -79,22 +79,10 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'faces',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'postgres',
-        # 'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
